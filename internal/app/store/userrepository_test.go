ae451aa03574ad7157ab9d750e2533e4b418a5fc
reed665 <abuiko@outlook.com>
2020-05-03 09:58:39 +0300

    add User model and UserRepository and some tests

---
+++ internal/app/store/userrepository_test.go
@@ -0,0 +1,21 @@
+package store_test
+
+import (
+	"testing"
+
+	"github.com/reed665/http-rest-api/internal/app/model"
+	"github.com/reed665/http-rest-api/internal/app/store"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	s, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users")
+
+	u, err := s.User().Create(&model.User{
+		Email: "user@example.org",
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
