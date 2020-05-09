ba8f44640922a92bef1dc8084daebe8d2c534adf
reed665 <abuiko@outlook.com>
2020-05-09 11:00:42 +0300

    sqlstore userrepository Find and apiserver server authenticateUser

--- internal/app/store/sqlstore/userrepository_test.go
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -20,6 +20,23 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NotNil(t, u)
 }
 
+func TestUserRepository_Find(t *testing.T) {
+	db, teardown := sqlstore.TestDB(t, conf.DatabaseURL)
+	defer teardown("users")
+
+	s := sqlstore.New(db)
+	id := 123
+
+	_, err := s.User().Find(id)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
+
+	u1 := model.TestUser(t)
+	s.User().Create(u1)
+	u2, err := s.User().Find(u1.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, u2)
+}
+
 func TestUserRepository_FindByEmail(t *testing.T) {
 	db, teardown := sqlstore.TestDB(t, conf.DatabaseURL)
 	defer teardown("users")
