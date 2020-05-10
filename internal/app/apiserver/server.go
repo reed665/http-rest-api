c38ec70d48d2c5ee5815b4d6878a8e3209db39bc
reed665 <abuiko@outlook.com>
2020-05-10 11:58:33 +0300

    log status code using custom responseWriter

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -83,9 +83,15 @@ func (s *server) logRequest(next http.Handler) http.Handler {
 		logger.Infof("started %s %s", r.Method, r.RequestURI)
 
 		start := time.Now()
-		next.ServeHTTP(w, r)
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
 
-		logger.Infof("completed in %v", time.Now().Sub(start))
+		logger.Infof(
+			"completed with %d %s in %v",
+			rw.code,
+			http.StatusText(rw.code),
+			time.Now().Sub(start),
+		)
 	})
 }
 
